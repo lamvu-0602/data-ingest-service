4bda528c1c183c34248b30f649411a0b0aecdbfe
Vu Tung Lam <lamvu@kms-technology.com>
2026-05-25 11:52:23 +0700

    feature: initial source code

---
+++ src/test/java/kms/rooftop/data_ingest_service/DataIngestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kms.rooftop.data_ingest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIngestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
